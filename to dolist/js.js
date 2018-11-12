02232d4a23d1020cf638f0472ae38549e8aa1d0f
marwa1100 <33814205+marwa1100@users.noreply.github.com>
2018-11-12 20:09:20 +0200

    Add files via upload

--- to dolist/js.js
+++ to dolist/js.js
@@ -4,17 +4,27 @@ let arr = [];
 
 var text = document.querySelector(".toDo");
 var btn = document.querySelector(".btn");
-var parent = document.querySelector(".box");
+var fragment = document.createDocumentFragment();
+var ul = document.querySelector(".ul");
+var span1 = document.querySelector(".span1");
+var span2 = document.querySelector(".span2");
 btn.addEventListener("click", action);
+
 function action(){
+let li = document.createElement("li");
 let todo = text.value;
  arr.push(todo);
-console.log(arr);
-
-
-parent.append(todo);
+span2.textContent = arr.length;
+li.textContent = todo;
+fragment.appendChild(li);
+ul.append(fragment);
+li.addEventListener("click", style)
 }
-
-
 }
 newNote();
+function style(){
+  this.classList.toggle("line");
+  var span1 = document.querySelector(".span1");
+
+  span1.textContent = document.querySelectorAll(".line").length;
+}
